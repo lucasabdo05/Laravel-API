54517da6af5bab77fab9cf0b602694735b865f7e
Lucas Abdo <lucarioales@gmail.com>
2023-06-30 19:46:44 -0300

    Tasks table migration criada

--- database/migrations/2023_06_30_205623_create_tasks_table.php
+++ database/migrations/2023_06_30_205623_create_tasks_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->enum('status', ['concluida', 'nao_concluida']);
         });
     }
 
