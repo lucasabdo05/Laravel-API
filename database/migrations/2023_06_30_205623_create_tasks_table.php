a4ae635eaffbaac2de13fa50a5bf707bae00faa8
Lucas Abdo <lucarioales@gmail.com>
2023-07-02 14:27:08 -0300

    Arquivo create_tasks_table atualizado, agora a rota post está funcionando

--- database/migrations/2023_06_30_205623_create_tasks_table.php
+++ database/migrations/2023_06_30_205623_create_tasks_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('titulo');
+            $table->string('titulo')->nullable();
             $table->text('descricao');
             $table->enum('status', ['concluida', 'nao_concluida']);
         });
