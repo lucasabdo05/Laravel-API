87883ed2db83e82fd0d219e1100a880e7b17e66c
Lucas Abdo <lucarioales@gmail.com>
2023-07-02 13:35:55 -0300

    Problema do CsrfToken corrigido e Model com melhor visualização

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,9 +9,5 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'titulo',
-        'descricao',
-        'status',
-    ];
+    protected $fillable = ['titulo', 'descricao', 'status'];
 }
